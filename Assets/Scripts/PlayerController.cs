88387480541236843ccee1ee8889e2e858abed7c
ydbr0918 <inseo060918@g.shingu.ac.kr>
2025-09-15 15:20:32 +0900

    [Feat] 플레이어 점프 및 공격모션

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,12 @@ public class PlayerController : MonoBehaviour
     public float runSpeed = 6f;
     public float rotationSpeed = 10;
 
+    [Header("점프 설정")]
+    public float jumpHeight = 2f;
+    public float gravity = -9.81f;                         //중력 속도 추가
+    public float landingDuration = 0.3f;                   //착지 후 착지 지속시간
+
+
     [Header("공격 설정")]
     public float attackDuration = 0.8f;                              //공격 지속 시간
     public bool canMoveWhileAttacking = false;                       //공격중 이동 가능 여부
@@ -22,6 +28,13 @@ public class PlayerController : MonoBehaviour
     //현재 상태
     private float currentSpeed;
     private bool isAttacking = false;                                 //공격중인지 체크
+    private bool isLanding = false;                                  //착지 중인지 확인
+    private float landingTimer;                                      //착지 타이머
+
+    private Vector3 velocity;
+    private bool isGrounded;
+    private bool wasGrounded;                                        //이전 프레임이 땅 이였는지
+    private float attackTimer;
 
     // Start is called before the first frame update
     void Start()
@@ -33,12 +46,68 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CheckGrounded();
+        HandleLanding();
         HandleMovement();
+        HandleJump();
+        HandleAttack();
         UpdateAnimator();
+    }
+
+    void CheckGrounded()
+    {
+        //이전 상태 저장
+        wasGrounded = isGrounded;
+        isGrounded = controller.isGrounded;                      //캐릭터 컨트롤러에서 받아온다
+
+        if (!isGrounded && wasGrounded)                             //땅에서 떨어졌을때(지금 프레임은 땅이 아니고, 이전 프레임은 땅)
+        {
+            Debug.Log("떨어지기 시작");
+        }
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+
+            //착지 모션 트리거 및 착지 상태 시작
+            if (!wasGrounded && animator != null)
+            {
+                //animator.SetTrigger("landTrigger");
+                isLanding = true;
+                landingTimer = landingDuration;
+                Debug.Log("착지");
+            }
+        }
+    }
+
+    void HandleJump()
+    {
+        if (Input.GetButton("Jump") && isGrounded)       //땅 위에 있을때만 점프를 할 수 있다
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+            if (animator != null)
+            {
+                animator.SetTrigger("jumpTrigger");
+            }
+        }
+
+        if (!isGrounded)                                    //땅에 있지 않을 경우 중력 정용
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+
+        controller.Move(velocity * Time.deltaTime);
     }
 
     void HandleMovement()              //이동 함수 제작
     {
+        if ((isAttacking && !canMoveWhileAttacking) || isLanding)
+        {
+            currentSpeed = 0;
+            return;
+        }
+
         float horizontal = Input.GetAxis("Horizontal");
         float verical = Input.GetAxis("Vertical");
 
@@ -75,10 +144,51 @@ public class PlayerController : MonoBehaviour
 
     }
 
+    void HandleLanding()
+    {
+        if (isLanding)
+        {
+            landingTimer -= Time.deltaTime;
+            if (landingTimer <= 0)
+            {
+                isLanding = false;
+            }
+        }
+
+    }
+
+    void HandleAttack()
+    {
+        if (isAttacking)
+        {
+            attackTimer -= Time.deltaTime;
+            if (attackTimer <= 0)
+            {
+                isAttacking = false;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1) && !isAttacking)
+        {
+            isAttacking = true;
+            attackTimer = attackDuration;
+
+            if (animator != null)
+            {
+                animator.SetTrigger("attackTrigger");
+            }
+        }
+    }
+
     void UpdateAnimator()
     {
         //전체 최대 속도(runSpeed) 기준으로 0~1 계산
         float animatorSpeed = Mathf.Clamp01(currentSpeed / runSpeed);
         animator.SetFloat("Speed", animatorSpeed);
+        animator.SetBool("isGrounded", isGrounded);
+
+        bool isFalling = !isGrounded && velocity.y < -0.1f;             //캐릭터의 Y축 속도가 음수로 넘어가면 떨어지고 있다고 판단
+        animator.SetBool("isFalling", isFalling);
+        animator.SetBool("isLanding", isLanding);
+
     }
 }
